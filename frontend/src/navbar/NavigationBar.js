616a30894cd70dbbdb8d3801c06b5f10c12ac285
Ping GAO <ping.gao@student.unsw.edu.au>
2020-10-28 02:18:31 +1100

    set up

--- frontend/src/navbar/NavigationBar.js
+++ frontend/src/navbar/NavigationBar.js
@@ -3,22 +3,25 @@ import { Navbar, NavbarBrand, Nav, NavItem, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./NavigationBar.css";
 const NavigationBar = () => {
+  // a div contianer added for future usage
   return (
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="/home">Home</NavbarBrand>
-      <Nav className="ml-auto" navbar>
-        <NavItem className="mx-2">
-          <Button outline color="primary">
-            <Link to="/signup">Signup</Link>
-          </Button>
-        </NavItem>
-        <NavItem>
-          <Button outline color="primary">
-            <Link to="/login">Login</Link>
-          </Button>
-        </NavItem>
-      </Nav>
-    </Navbar>
+    <div className="container-nav">
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/home">Home</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem className="mx-2">
+            <Button outline color="primary">
+              <Link to="/signup">Signup</Link>
+            </Button>
+          </NavItem>
+          <NavItem>
+            <Button outline color="primary">
+              <Link to="/login">Login</Link>
+            </Button>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 
