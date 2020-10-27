ef4622d8adf768bd9f396d376c023142d53bfcd0
Ping GAO <ping.gao@student.unsw.edu.au>
2020-10-28 02:29:58 +1100

    add stub login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from './login/Login';
-import Signup from './signup/Signup';
-import Home from './home/Home';
-import NavigationBar from "./navbar/NavigationBar";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+import Home from "./components/home/Home";
+import NavigationBar from "./components/navbar/NavigationBar";
 const App = () => {
-	return (
-		<Router>
-			<NavigationBar />
-			<Switch>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/signup">
-					<Signup />
-				</Route>
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <NavigationBar />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
-
 
 export default App;
