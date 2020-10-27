01534df674efd3cf10dd9c5bb9df6682ec13afaa
Ping GAO <ping.gao@student.unsw.edu.au>
2020-10-28 02:09:28 +1100

    add some css holder gile

--- frontend/src/navbar/NavigationBar.js
+++ frontend/src/navbar/NavigationBar.js
@@ -1,27 +1,24 @@
 import React from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, Button } from "reactstrap";
 import { Link } from "react-router-dom";
-
+import "./NavigationBar.css";
 const NavigationBar = () => {
   return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/home">reactstrap</NavbarBrand>
-
-        <Nav className="ml-auto" navbar>
-          <NavItem className="mx-2">
-            <Button outline color="primary">
-              <Link to="/signup">Signup</Link>
-            </Button>
-          </NavItem>
-          <NavItem>
-            <Button outline color="primary">
-              <Link to="/login">Login</Link>
-            </Button>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </div>
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/home">Home</NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavItem className="mx-2">
+          <Button outline color="primary">
+            <Link to="/signup">Signup</Link>
+          </Button>
+        </NavItem>
+        <NavItem>
+          <Button outline color="primary">
+            <Link to="/login">Login</Link>
+          </Button>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 };
 
