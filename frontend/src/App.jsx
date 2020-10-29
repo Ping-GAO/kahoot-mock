486bc7fb9ed40b779f63705ccab1149453343164
Ping Gao <ping.gao@student.unsw.edu.au>
2020-10-29 17:48:32 +1100

    fix all linting problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Home from "./components/home/Home";
 import NavigationBar from "./components/navbar/NavigationBar";
+
 const App = () => {
   return (
     <Router>
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,2 @@
+export const placeHolder = "Make linter stut up";
 export const API_URL = "http://localhost:5005/";
