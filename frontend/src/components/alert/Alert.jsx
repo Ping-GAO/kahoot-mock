89d81ea25b77be9ba794903612ccd68258dcca24
Ping Gao <ping.gao@student.unsw.edu.au>
2020-11-01 05:16:00 +1100

    format file

--- frontend/src/components/alert/Alert.jsx
+++ frontend/src/components/alert/Alert.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import MuiAlert from '@material-ui/lab/Alert';
 import Snackbar from "@material-ui/core/Snackbar";
-
 import PropTypes from 'prop-types';
-
-
-
 
 
 const CustomizedSnackbars = ({type,message}) => {
     const [open, setOpen] = useState(true);
-    const handleClose = (event, reason) => {
+    const handleClose = (_event, reason) => {
         if (reason === "clickaway") {
             return;
         }
@@ -20,15 +16,16 @@ const CustomizedSnackbars = ({type,message}) => {
         anchorOrigin={{
             vertical: 'top',
             horizontal: 'center',
-        }}
-    >
+        }}>
         <MuiAlert elevation={6}
             variant="filled"
             onClose={handleClose}
             severity={type} >
-            {message}</MuiAlert>
+            {message}
+        </MuiAlert>
     </Snackbar>;
 }
+
 CustomizedSnackbars.propTypes = {
     type: PropTypes.string.isRequired,
     message: PropTypes.string.isRequired
