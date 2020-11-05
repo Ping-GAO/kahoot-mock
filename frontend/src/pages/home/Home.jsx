dc75d386fe25928d461ff8c6682dc37ef903c242
Emma Paris <eparis@atlassian.com>
2020-11-05 17:58:29 +1100

    'meger'

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
 const Home = () => {
     const loginStatus = useSelector((state) => state.authentication);
+    const mystyle = {
+        backgroundImage: `url(${'https://source.unsplash.com/random'})`,
+        height: '100%',
+        width:'100%',
+        position: 'absolute',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        color: 'white',
+        display:'flex'
+    }
+    const textstyle={
+        margin:'auto',
+        textAlign:'center',
+        fontSize:'50px',
+        fontWeight:'bold'
+    }
     return (
-        <div className="container-home">
-            {loginStatus.loggedIn ? "Already Logged in" : "Not Log in"}
+        <div style={mystyle} className="container-home">
+            <div style={textstyle} className='text'>
+                {loginStatus.loggedIn ? "Already Logged in" : "Welcome to the BigBrain Game, please log in to play."}</div>
         </div>
     );
 };
