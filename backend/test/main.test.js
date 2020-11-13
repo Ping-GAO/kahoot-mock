15c66f8031a9b624ff19e14c4ea60b54406b4cb7
Ping Gao <ping.gao@student.unsw.edu.au>
2020-11-14 02:50:52 +1100

    merged income change

--- backend/test/main.test.js
+++ backend/test/main.test.js
@@ -2,27 +2,28 @@ import request from 'supertest';
 import server from '../src/server';
 import { reset, } from '../src/service';
 
-const THUMBNAIL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
-const QUESTIONS = [
-  {
-
-  },
-  {
-
-  },
-  {
-
-  },
-];
+const THUMBNAIL =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+const QUESTIONS = [{}, {}, {},];
 const PLAYERS = ['HAYDEN1', 'HAYDEN2', 'HAYDEN3', 'HAYDEN4', 'HAYDEN5',];
 const PLAYER_IDS = [];
 
-const postTry = async (path, status, payload, token) => sendTry('post', path, status, payload, token);
-const getTry = async (path, status, payload, token) => sendTry('get', path, status, payload, token);
-const deleteTry = async (path, status, payload, token) => sendTry('delete', path, status, payload, token);
-const putTry = async (path, status, payload, token) => sendTry('put', path, status, payload, token);
+const postTry = async (path, status, payload, token) =>
+  sendTry('post', path, status, payload, token);
+const getTry = async (path, status, payload, token) =>
+  sendTry('get', path, status, payload, token);
+const deleteTry = async (path, status, payload, token) =>
+  sendTry('delete', path, status, payload, token);
+const putTry = async (path, status, payload, token) =>
+  sendTry('put', path, status, payload, token);
 
-const sendTry = async (typeFn, path, status = 200, payload = {}, token = null) => {
+const sendTry = async (
+  typeFn,
+  path,
+  status = 200,
+  payload = {},
+  token = null
+) => {
   let req = request(server);
   if (typeFn === 'post') {
     req = req.post(path);
@@ -58,7 +59,12 @@ const singleQuizId = async () => {
 const singleSessionStatus = async () => {
   const { quizzes, } = await getTry('/admin/quiz', 200, {}, await validToken());
   const sessionid = quizzes[0].active;
-  const body = await getTry(`/admin/session/${sessionid}/status`, 200, {}, await validToken());
+  const body = await getTry(
+    `/admin/session/${sessionid}/status`,
+    200,
+    {},
+    await validToken()
+  );
   return body.results;
 };
 
@@ -120,7 +126,12 @@ describe('Test the root path', () => {
   });
 
   test('Logout a valid session', async () => {
-    const bodyLogout = await postTry('/admin/auth/logout', 200, {}, await validToken());
+    const bodyLogout = await postTry(
+      '/admin/auth/logout',
+      200,
+      {},
+      await validToken()
+    );
     expect(bodyLogout).toMatchObject({});
   });
 
@@ -148,9 +159,14 @@ describe('Test the root path', () => {
   });
 
   test('Creating a single quiz', async () => {
-    const body = await postTry('/admin/quiz/new', 200, {
-      name: 'QUIZ',
-    }, await validToken());
+    const body = await postTry(
+      '/admin/quiz/new',
+      200,
+      {
+        name: 'QUIZ',
+      },
+      await validToken()
+    );
   });
 
   test('That there is now one quiz', async () => {
@@ -166,9 +182,14 @@ describe('Test the root path', () => {
   });
 
   test('Create a second quiz', async () => {
-    const body = await postTry('/admin/quiz/new', 200, {
-      name: 'QUIZ2',
-    }, await validToken());
+    const body = await postTry(
+      '/admin/quiz/new',
+      200,
+      {
+        name: 'QUIZ2',
+      },
+      await validToken()
+    );
   });
 
   test('That there is now two quizzes', async () => {
@@ -177,19 +198,39 @@ describe('Test the root path', () => {
   });
 
   test('Try and delete a quiz with invalid token', async () => {
-    const { quizzes, } = await getTry('/admin/quiz', 200, {}, await validToken());
+    const { quizzes, } = await getTry(
+      '/admin/quiz',
+      200,
+      {},
+      await validToken()
+    );
     const quizid = quizzes[1].id;
     const body = await deleteTry(`/admin/quiz/${quizid}`, 403, {});
   });
 
   test('Try and delete a quiz with invalid quizid', async () => {
-    const body = await deleteTry(`/admin/quiz/${99999999999999}`, 400, {}, await validToken());
+    const body = await deleteTry(
+      `/admin/quiz/${99999999999999}`,
+      400,
+      {},
+      await validToken()
+    );
   });
 
   test('Try and delete a quiz', async () => {
-    const { quizzes, } = await getTry('/admin/quiz', 200, {}, await validToken());
+    const { quizzes, } = await getTry(
+      '/admin/quiz',
+      200,
+      {},
+      await validToken()
+    );
     const quizid = quizzes[1].id;
-    const body = await deleteTry(`/admin/quiz/${quizid}`, 200, {}, await validToken());
+    const body = await deleteTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {},
+      await validToken()
+    );
   });
 
   test('That there is now one quiz again', async () => {
@@ -199,16 +240,26 @@ describe('Test the root path', () => {
 
   test('Update quiz thumbnail and name', async () => {
     const quizid = await singleQuizId();
-    const body = await putTry(`/admin/quiz/${quizid}`, 200, {
-      name: 'QUIZDIFF',
-      thumbnail: THUMBNAIL,
-      questions: QUESTIONS,
-    }, await validToken());
+    const body = await putTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {
+        name: 'QUIZDIFF',
+        thumbnail: THUMBNAIL,
+        questions: QUESTIONS,
+      },
+      await validToken()
+    );
   });
 
   test('Check that thumbnail and name updated', async () => {
     const quizid = await singleQuizId();
-    const quiz = await getTry(`/admin/quiz/${quizid}`, 200, {}, await validToken());
+    const quiz = await getTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {},
+      await validToken()
+    );
     expect(quiz.name).toBe('QUIZDIFF');
     expect(quiz.thumbnail).toBe(THUMBNAIL);
     expect(quiz.questions).toMatchObject(QUESTIONS);
@@ -218,38 +269,42 @@ describe('Test the root path', () => {
                        Admin Running a Session
   ***************************************************************/
 
-  test('Can\'t start a session with invalid token', async () => {
+  test("Can't start a session with invalid token", async () => {
     const body = await postTry('/admin/quiz/123/start', 403, {});
   });
 
-  test('Can\'t advance a session with invalid token', async () => {
+  test("Can't advance a session with invalid token", async () => {
     const body = await postTry('/admin/quiz/123/advance', 403, {});
   });
 
-  test('Can\'t end a session with invalid token', async () => {
+  test("Can't end a session with invalid token", async () => {
     const body = await postTry('/admin/quiz/123/end', 403, {});
   });
 
-  test('Can\'t get session status with invalid token', async () => {
+  test("Can't get session status with invalid token", async () => {
     const body = await getTry('/admin/session/123/status', 403, {});
   });
 
-  test('Can\'t get session results with invalid token', async () => {
+  test("Can't get session results with invalid token", async () => {
     const body = await getTry('/admin/session/123/results', 403, {});
   });
 
-  test('Ensure a session cant be ended when hasn\'t started', async () => {
+  test("Ensure a session cant be ended when hasn't started", async () => {
     const quizid = await singleQuizId();
     await postTry(`/admin/quiz/${quizid}/end`, 400, {}, await validToken());
   });
 
-  test('Ensure a session cant be advanced when hasn\'t started', async () => {
+  test("Ensure a session cant be advanced when hasn't started", async () => {
     const quizid = await singleQuizId();
     await postTry(`/admin/quiz/${quizid}/advance`, 400, {}, await validToken());
   });
 
-  test('Can\'t start a quiz with invalid quizid', async () => {
-    const body = await postTry('/admin/quiz/99999999999/start', 403, await validToken());
+  test("Can't start a quiz with invalid quizid", async () => {
+    const body = await postTry(
+      '/admin/quiz/99999999999/start',
+      403,
+      await validToken()
+    );
   });
 
   test('Ensure a quiz can be started', async () => {
@@ -259,7 +314,12 @@ describe('Test the root path', () => {
 
   test('A session now exists for the quiz', async () => {
     const quizid = await singleQuizId();
-    const body = await getTry(`/admin/quiz/${quizid}`, 200, {}, await validToken());
+    const body = await getTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {},
+      await validToken()
+    );
     expect(typeof body.active).toBe('number');
   });
 
@@ -267,6 +327,7 @@ describe('Test the root path', () => {
     const status = await singleSessionStatus();
     expect(status.active).toBe(true);
     expect(status.answerAvailable).toBe(false);
+    expect(status.isoTimeLastQuestionStarted).toBe(null);
     expect(status.position).toBe(-1);
     expect(status.questions).toMatchObject(QUESTIONS);
     expect(status.players).toMatchObject([]);
@@ -276,12 +337,12 @@ describe('Test the root path', () => {
                        Try Playing
   ***************************************************************/
 
-  test('Test player can\'t join without a name', async () => {
+  test("Test player can't join without a name", async () => {
     const sessionId = await singleSessionId();
     await postTry(`/play/join/${sessionId}`, 400, {});
   });
 
-  test('Test player can\'t join without a valid session ID', async () => {
+  test("Test player can't join without a valid session ID", async () => {
     await postTry(`/play/join/${9999999999999}`, 400, {
       name: 'HAYDEN',
     });
@@ -331,7 +392,12 @@ describe('Test the root path', () => {
   for (const questionPosition in QUESTIONS) {
     test('Try to advance the quiz', async () => {
       const quizid = await singleQuizId();
-      await postTry(`/admin/quiz/${quizid}/advance`, 200, {}, await validToken());
+      await postTry(
+        `/admin/quiz/${quizid}/advance`,
+        200,
+        {},
+        await validToken()
+      );
     });
 
     test('Session position has increased', async () => {
@@ -339,6 +405,7 @@ describe('Test the root path', () => {
       expect(status.position).toBe(parseInt(questionPosition, 10));
       expect(status.active).toBe(true);
       expect(status.answerAvailable).toBe(false);
+      expect(typeof status.isoTimeLastQuestionStarted).toBe('string');
     });
 
     test('Players fail to submit no answers', async () => {
@@ -351,7 +418,7 @@ describe('Test the root path', () => {
     test('Players attempt to submit answer', async () => {
       for (const playerId of PLAYER_IDS) {
         const payload = {
-          answerIds: [0, 1, 2, ],
+          answerIds: [0, 1, 2,],
         };
         const body = await putTry(`/play/${playerId}/answer`, 200, payload);
         expect(body).toMatchObject({});
@@ -374,15 +441,26 @@ describe('Test the root path', () => {
     const sessionId = await singleSessionId();
     const quizid = await singleQuizId();
     await postTry(`/admin/quiz/${quizid}/advance`, 200, {}, await validToken());
-    const { results, } = await getTry(`/admin/session/${sessionId}/status`, 200, {}, await validToken());
+    const { results, } = await getTry(
+      `/admin/session/${sessionId}/status`,
+      200,
+      {},
+      await validToken()
+    );
     expect(results.position).toBe(QUESTIONS.length);
     expect(results.active).toBe(false);
     expect(results.answerAvailable).toBe(false);
+    expect(typeof results.isoTimeLastQuestionStarted).toBe('string');
   });
 
   test('Ensure that the session appears in old sessions', async () => {
     const quizid = await singleQuizId();
-    const { oldSessions, } = await getTry(`/admin/quiz/${quizid}`, 200, {}, await validToken());
+    const { oldSessions, } = await getTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {},
+      await validToken()
+    );
     expect(oldSessions).toHaveLength(1);
   });
 
@@ -399,13 +477,25 @@ describe('Test the root path', () => {
   test('Players should be able to get their results', async () => {
     for (const playerId of PLAYER_IDS) {
       const body = await getTry(`/play/${playerId}/results`, 200);
-      expect(body).toMatchObject(QUESTIONS.map(q => ({ answerIds: [0, 1, 2, ], correct: false, })));
+      expect(body).toMatchObject(
+        QUESTIONS.map((q) => ({ answerIds: [0, 1, 2,], correct: false, }))
+      );
     }
   });
 
   test('Admins can get the session results', async () => {
     const quizid = await singleQuizId();
-    const { oldSessions, } = await getTry(`/admin/quiz/${quizid}`, 200, {}, await validToken());
-    const body = await getTry(`/admin/session/${oldSessions[0]}/results`, 200, {}, await validToken());
+    const { oldSessions, } = await getTry(
+      `/admin/quiz/${quizid}`,
+      200,
+      {},
+      await validToken()
+    );
+    const body = await getTry(
+      `/admin/session/${oldSessions[0]}/results`,
+      200,
+      {},
+      await validToken()
+    );
   });
 });
