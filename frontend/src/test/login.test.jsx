8ab7a16e60d86ec42e4c3d19e5fe42478fd59e95
Ping GAO <ping.gao@student.unsw.edu.au>
2020-11-18 01:34:03 +1100

    fix pacakge install problem

--- frontend/src/test/login.test.jsx
+++ frontend/src/test/login.test.jsx
@@ -2,10 +2,9 @@ import shallow from "enzyme/src/shallow";
 import React from "react";
 import Login from "../pages/login/Login";
 
-
 describe('Button', () => {
-    const noop = () => {
-    };
+    // const noop = () => {
+    // };
     it('triggers onClick event handler when clicked', () => {
         const onClick = jest.fn();
         shallow(<Login onClick={onClick}/>).simulate('click');
