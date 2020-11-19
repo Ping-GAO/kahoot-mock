0be264a3aba431a326104825f01af541dcd64c12
Ping GAO <ping.gao@student.unsw.edu.au>
2020-11-20 00:05:07 +1100

    fix linting problem

--- frontend/src/test/signup.test.js
+++ frontend/src/test/signup.test.js
@@ -1,10 +1,10 @@
 // test signup button
 import {mount} from "enzyme";
 import {Provider} from "react-redux";
-import store from "../redux/stores";
-import Signup from "../pages/signup/Signup";
 import Button from "@material-ui/core/Button";
 import React from "react";
+import store from "../redux/stores";
+import Signup from "../pages/signup/Signup";
 
 describe('Signup Page', () => {
     it('Signup page should have a button to signup', () => {
