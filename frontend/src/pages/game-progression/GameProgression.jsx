6ee76f79ecf196309a9d5f6964c9b278ed9bf40c
Ping GAO <ping.gao@student.unsw.edu.au>
2020-11-19 17:55:14 +1100

    .

--- frontend/src/pages/game-progression/GameProgression.jsx
+++ frontend/src/pages/game-progression/GameProgression.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { useDispatch } from "react-redux";
@@ -9,8 +9,9 @@ const GameProgression = () => {
     const { quizId, sessionId } = useParams();
     const dispatch = useDispatch();
     // -2 is an impossible value for fetch to return
-    // this is set to default value 
-    const [position,setPosition] = useState(-2);
+    // this is set to default value
+    const [position, setPosition] = useState(-2);
+    const [gameLength,setGameLength] = useState(-1);
     useEffect(() => {
         fetch(`${API_URL}/admin/session/${sessionId}/status`, {
             method: "GET",
@@ -20,12 +21,10 @@ const GameProgression = () => {
         })
             .then((res) => res.json())
             .then((data) => {
-            
-               
-                const {results} = data;
-                console.log(results)
+                const { results } = data;
+                console.log(results);
                 setPosition(results.position);
-            
+                setGameLength(results.questions.length);
             });
     }, [sessionId]);
 
@@ -48,38 +47,43 @@ const GameProgression = () => {
             .then(
                 () => {
                     dispatch(alertSuccess("Advance Sucess"));
+                    console.log("length",gameLength);
+                   
+                    setPosition(prevPosition => prevPosition+1);
+                    
+                    
                 },
                 (error) => {
                     dispatch(alertError(error));
                 }
             );
     };
-    console.log(position);
-    
-    
-    let  pageContent;
-    if(position===-2){
-        return null; 
+    // console.log(position,gameLength);
+
+    let pageContent;
+    if (position === -2) {
+        return null;
     }
-    if(position===-1){
-        pageContent =  (   <Button color="primary" onClick={handleAdvanceGame}>
+    if (position === -1) {
+        pageContent = (
+            <Button color="primary" onClick={handleAdvanceGame}>
         Start The Game
-        </Button>);
-    
+            </Button>
+        );
+        // use setTimeout to get the answer then refresh the page by changing state varible
+    } else if(position===gameLength-1){
+        // didn't end now, end with a timeout
+        pageContent = (<div>Game End</div>);
     }
     else{
-        pageContent =  (   <Button color="primary" onClick={handleAdvanceGame}>
-        Advance 
-        </Button>)
-        
     
+        pageContent = (
+            <Button color="primary" onClick={handleAdvanceGame}>
+        Advance
+            </Button>
+        );
     }
-    
-    
-    
-    
-    
-    
+
     return (
         <>
             <div>sessionId:{sessionId}</div>
