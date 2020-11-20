a6f9f5f4c1d22a092684f72a915e6287798a484e
Ping GAO <ping.gao@student.unsw.edu.au>
2020-11-20 22:07:32 +1100

    work on test

--- frontend/src/test/login.test.js
+++ frontend/src/test/login.test.js
@@ -2,20 +2,23 @@ import {mount} from 'enzyme';
 import React from "react";
 import {Provider} from "react-redux";
 import Button from "@material-ui/core/Button";
-
 import TextField from "@material-ui/core/TextField";
 import store from "../redux/stores";
-
 import Login from "../pages/login/Login";
 
 
 // test login page
 describe('Login Page', () => {
+
+
+    const wrapper = mount(
+        <Provider store={store}>
+            <Login/>
+        </Provider>);
+        
+        
     it('login page should have a button to login', () => {
-        const wrapper = mount(
-            <Provider store={store}>
-                <Login/>
-            </Provider>);
+       
         const loginPage = wrapper.children();
         const loginButton = loginPage.find(Button);
         expect(loginButton.text()).toBe("Log in");
@@ -23,18 +26,12 @@ describe('Login Page', () => {
     })
 
     it('login page has a textfield to input user email', () => {
-        const component = mount(
-            <Provider store={store}>
-                <Login/>
-            </Provider>);
-        expect(component.find(TextField).first().text()).toBe("Email *");
+      
+        expect(wrapper.find(TextField).first().text()).toBe("Email *");
     });
 
     it('login page has a textfield to input user password ', () => {
-        const component = mount(
-            <Provider store={store}>
-                <Login/>
-            </Provider>);
-        expect(component.find(TextField).last().text()).toBe("Password *");
+      
+        expect(wrapper.find(TextField).last().text()).toBe("Password *");
     });
 })
