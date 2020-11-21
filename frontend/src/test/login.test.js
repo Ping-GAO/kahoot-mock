fcd3ff65256bdcad707ac754e4f93630834732d8
Ping GAO <ping.gao@student.unsw.edu.au>
2020-11-22 00:15:33 +1100

    set up test

--- frontend/src/test/login.test.js
+++ frontend/src/test/login.test.js
@@ -11,27 +11,56 @@ import Login from "../pages/login/Login";
 describe('Login Page', () => {
 
 
-    const wrapper = mount(
-        <Provider store={store}>
-            <Login/>
-        </Provider>);
-        
-        
-    it('login page should have a button to login', () => {
+    let loginPage;
+    let wrapper;
+    beforeEach(()=>{
+        wrapper= mount(
+            <Provider store={store}>
+                <Login/>
+            </Provider>);
+        loginPage = wrapper.children();
+    });
+    afterEach(() => {
+        wrapper.unmount();
+	 });
+
+    
+    it('render a button to login', () => {
        
-        const loginPage = wrapper.children();
+       
         const loginButton = loginPage.find(Button);
         expect(loginButton.text()).toBe("Log in");
         expect(loginButton).toMatchSnapshot();
     })
 
-    it('login page has a textfield to input user email', () => {
+    it('render a textfield to input user email', () => {
       
-        expect(wrapper.find(TextField).first().text()).toBe("Email *");
+        expect(loginPage.find(TextField).first().text()).toBe("Email *");
     });
 
-    it('login page has a textfield to input user password ', () => {
+    it('render a textfield to input user password ', () => {
       
-        expect(wrapper.find(TextField).last().text()).toBe("Password *");
+        expect(loginPage.find(TextField).last().text()).toBe("Password *");
     });
+    
+    
+
+    describe('my sweet test', () => {
+        it('clicks it', () => {
+		  
+		   
+		   console.log();
+            //    const spy = jest.spyOn(instance, 'myClickFunc')
+	   
+            //    instance.forceUpdate();    
+	   
+            //    const p = app.find('.App-intro')
+            //    p.simulate('click')
+            //    expect(spy).toHaveBeenCalled()
+        })
+	   })
+	
+
+        
+        
 })
